d0a6c4d74d263626ddada0f84e50a7ef3b3467d3
Ayodele Oniosun <ayodeleoniosun63@gmail.com>
2024-07-22 19:33:16 +0100

    feat: update bash setup

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ docker-compose build
 # Spring up docker containers in detached mode
 docker-compose up -d --force-recreate
 
+echo "${PINK}Running migrations ..."
+
 docker-compose exec app npx sequelize-cli db:migrate
 
 echo "${GREEN} Application dockerized and started"
